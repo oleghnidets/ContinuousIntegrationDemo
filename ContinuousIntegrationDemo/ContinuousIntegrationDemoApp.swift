2f5ed535d07dfa224d81aa00553dcdb56639dac6
Oleg Gnidets <oleg.gnidets@tomtom.com>
2024-11-18 19:43:31 +0200

    Initial commit

---
+++ ContinuousIntegrationDemo/ContinuousIntegrationDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContinuousIntegrationDemoApp.swift
+//  ContinuousIntegrationDemo
+//
+//  Created by ognidets on 17.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct ContinuousIntegrationDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
